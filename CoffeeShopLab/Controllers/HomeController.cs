f85d219792cb1f9e2e7bd6a0ba2fdfecd6ad0d76
alice <aliceamarawell@gmail.com>
2021-05-11 14:05:58 -0700

    First commit

---
+++ CoffeeShopLab/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using CoffeeShopLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            
+            return View();
+        }
+        public IActionResult Summary(Registration register)
+        {
+            ViewData["FirstName"] = register.FirstName;
+            ViewData["LastName"] = register.LastName;
+            ViewData["Email"] = register.Email;
+            ViewData["Password"] = register.Password;
+
+            return View();
+        }
+
+        
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
